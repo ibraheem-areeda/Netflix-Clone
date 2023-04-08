a61e09775827e8d97fe4ffa039109b0633b1c84b
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-04-08 20:46:34 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import  Home  from "./components/Home/Home";
 import './App.css';
-
+import Header from "./components/Navbar/Navbar.js"
 function App() {
   return (
     <div >
+
+      <Header />
       <Home />
 
       
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+
+ import "../Navbar/Navbar.css"
+
+
+
+function Header(props) {
+    return (
+        <>
+
+            <header>
+                <h1>MOVIES</h1>
+                <nav>
+                <ul>
+                    <li><a href= "/" >HOME</a></li>
+                    <li><a href="favorite list ">FAVORITE LIST</a></li>
+                    <li><a href="new release">NEW RELEASE</a></li>
+                    <li><a href="ATTRACTIONS">TOP RATED</a></li>
+                </ul>
+            </nav>
+        </header>
+
+    </>
+    
+    
+    )
+
+
+}
+
+export default Header;
